639d621335ff2c3d5ce593a73a4f8420add3b8b6
Timothy Adamcik <tadamcik0425@gmail.com>
2019-10-27 13:55:59 -0400

    Initial Commit

---
+++ memeMe1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memeMe1.0
+//
+//  Created by Timothy Adamcik on 10/27/19.
+//  Copyright © 2019 Timothy Adamcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
