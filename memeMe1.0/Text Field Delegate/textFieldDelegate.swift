38b01fed4ff4641543cbbfac9deca19834848501
Timothy Adamcik <tadamcik0425@gmail.com>
2019-10-27 14:58:55 -0400

    text field delegate and memetextatrributes

--- memeMe1.0/Text Field Delegate/textFieldDelegate.swift
+++ memeMe1.0/Text Field Delegate/textFieldDelegate.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+
+        return true
+    }
+}
--- memeMe1.0/ViewController.swift
+++ memeMe1.0/ViewController.swift
@@ -15,8 +15,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
+    let textFieldDel = TextFieldDelegate()
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.topTextField.delegate = textFieldDel
+        self.bottomTextField.delegate = textFieldDel
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+                 NSAttributedString.Key.strokeColor : UIColor.black ,
+                 NSAttributedString.Key.foregroundColor: UIColor.white ,
+                 NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+                 NSAttributedString.Key.strokeWidth: -3.0
+             ]
+             
+             topTextField.defaultTextAttributes = memeTextAttributes
+             bottomTextField.defaultTextAttributes = memeTextAttributes
+             topTextField.textAlignment = .center
+             bottomTextField.textAlignment = .center
+             topTextField.backgroundColor = UIColor.clear
+             bottomTextField.backgroundColor = UIColor.clear
+            topTextField.borderStyle = .none
+            bottomTextField.borderStyle = .none
         // Do any additional setup after loading the view.
     }
     
