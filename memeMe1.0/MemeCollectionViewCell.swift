c622ad0000ca97b1ee136c5fd09c1f9a29583010
Timothy Adamcik <tadamcik0425@gmail.com>
2019-11-26 18:35:07 -0500

    added segue from tableview and collectionview to meme editor

--- memeMe1.0/MemeCollectionViewCell.swift
+++ memeMe1.0/MemeCollectionViewCell.swift
@@ -1,14 +1,14 @@
 //
-//  SentMemeCollectionViewCell.swift
+//  MemeCollectionViewCell.swift
 //  memeMe1.0
 //
-//  Created by Timothy Adamcik on 11/25/19.
+//  Created by Timothy Adamcik on 11/26/19.
 //  Copyright © 2019 Timothy Adamcik. All rights reserved.
 //
 
 import UIKit
 
-class SentMemeCollectionViewCell: UICollectionViewCell {
+class MemeCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var collectionMemeCell: UIImageView!
     
---
+++ memeMe1.0/MemeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemeTableViewCell.swift
+//  memeMe1.0
+//
+//  Created by Timothy Adamcik on 11/26/19.
+//  Copyright © 2019 Timothy Adamcik. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
