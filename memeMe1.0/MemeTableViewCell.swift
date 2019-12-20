f44e74c995efa6f403be87059c34b457825778b7
Timothy Adamcik <tadamcik0425@gmail.com>
2019-12-20 16:51:23 -0500

    initialized tableView label to be blank

--- memeMe1.0/MemeTableViewCell.swift
+++ memeMe1.0/MemeTableViewCell.swift
@@ -16,7 +16,8 @@ class MemeTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        savedMemeLabel.text = " "
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
