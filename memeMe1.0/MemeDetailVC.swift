7d7dab69a3fcf8ba356350a554212478748f8c91
Timothy Adamcik <tadamcik0425@gmail.com>
2019-12-29 20:43:28 -0500

    added meme detail VC and connected image and buttons

---
+++ memeMe1.0/MemeDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  MemeDetailVC.swift
+//  memeMe1.0
+//
+//  Created by Timothy Adamcik on 12/29/19.
+//  Copyright © 2019 Timothy Adamcik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeDetailVC: UIViewController {
+    
+    @IBOutlet weak var imageDetailView: UIImageView!
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    
+}
